95c4f9cddbae9229785e3c3ae4783d993ca8ff31
Eric <eric.lee.edl@gmail.com>
2014-01-31 16:57:40 -0500

    Added a color indicator to the color picker

--- MovieMood/SKYViewController.m
+++ MovieMood/SKYViewController.m
@@ -14,12 +14,14 @@
 @property NSDictionary *results;
 @property (nonatomic, retain) ISColorWheel *colorWheel;
 @property (nonatomic, retain) SKYColorAnalyser *colorAnalyser;
+@property (nonatomic, retain) UIView *colorIndicator;
 @end
 
 @implementation SKYViewController
 
 @synthesize colorWheel = _colorWheel;
 @synthesize colorAnalyser = _colorAnalyser;
+@synthesize colorIndicator = _colorIndicator;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -36,26 +38,37 @@
 	CGSize size = self.view.bounds.size;
     
     CGSize wheelSize = CGSizeMake(size.width * .75, size.width * .75);
-    CGSize indicatorSize = CGSizeMake(size.width * .50, size.width * .50);
+    CGSize centerWheelSize = CGSizeMake(size.width * .50, size.width * .50);
+    CGSize indicatorSize = CGSizeMake(size.width * .40, size.width * .40);
     
     
-    UIView *colorIndicator = [[UIView alloc] initWithFrame:CGRectMake(size.width / 2 - indicatorSize.width / 2,
-                                                                      (size.height * .3) + (wheelSize.height - indicatorSize.height) / 2,
-                                                                      indicatorSize.width,
-                                                                      indicatorSize.height)];
-    colorIndicator.backgroundColor = [UIColor colorWithRed:(77/255.0) green:(77/255.0) blue:(77/225.0) alpha:1.0];
-    colorIndicator.layer.cornerRadius = 80;
+    UIView *centerWheel = [[UIView alloc] initWithFrame:CGRectMake(size.width / 2 - centerWheelSize.width / 2,
+                                                                      (size.height * .3) + (wheelSize.height - centerWheelSize.height) / 2,
+                                                                      centerWheelSize.width,
+                                                                      centerWheelSize.height)];
+    centerWheel.backgroundColor = [UIColor colorWithRed:(77/255.0) green:(77/255.0) blue:(77/225.0) alpha:1.0];
+    centerWheel.layer.cornerRadius = 80;
+    centerWheel.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    centerWheel.layer.borderWidth = 1.f;
     
     _colorWheel = [[ISColorWheel alloc] initWithFrame:CGRectMake(size.width / 2 - wheelSize.width / 2,
                                                                  size.height * .3,
                                                                  wheelSize.width,
                                                                  wheelSize.height)];
     
+    _colorIndicator = [[UIView alloc] initWithFrame:CGRectMake(size.width / 2 - indicatorSize.width / 2,
+                                                               (size.height * .3) + (wheelSize.height - indicatorSize.height) / 2,
+                                                               indicatorSize.height,
+                                                               indicatorSize.width)];
+    _colorIndicator.layer.cornerRadius = 64;
+    _colorIndicator.backgroundColor = _colorWheel.currentColor;
+    
     _colorWheel.delegate = self;
     _colorWheel.continuous = true;
     _colorAnalyser = [[SKYColorAnalyser alloc] init];
     [self.view addSubview:_colorWheel];
-    [self.view addSubview:colorIndicator];
+    [self.view addSubview:centerWheel];
+    [self.view addSubview:_colorIndicator];
 }
 
 - (void)didReceiveMemoryWarning
@@ -94,6 +107,7 @@
 
 -(void)colorWheelDidChangeColor:(ISColorWheel *)colorWheel {
     [_colorAnalyser analyzeColor:colorWheel.currentColor];
+    _colorIndicator.backgroundColor = _colorWheel.currentColor;
 }
 
 @end
