f22a1e88d6c9d59b72707874fb9fc44aea65e12f
John Stelben <jstelben@icloud.com>
2014-02-01 17:24:02 -0500

    Added color changing

--- MovieMood/SKYResultViewController.h
+++ MovieMood/SKYResultViewController.h
@@ -10,5 +10,8 @@
 
 @interface SKYResultViewController : UITableViewController
 @property (nonatomic,retain) NSArray* movieSource;
+<<<<<<< HEAD
 @property (nonatomic, retain) UIColor* chosenColor;
+=======
+>>>>>>> 76815ba40e7826e2f6ea867fdb8d57089e73b1ca
 @end
