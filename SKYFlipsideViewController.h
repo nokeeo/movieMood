f942ff1accde3715d56de45a018c4d371d6a87a1
Aaron Sky <ans2799@rit.edu>
2014-02-01 18:47:12 -0500

    added flipside view controller for about/color key

---
+++ SKYFlipsideViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKYFlipsideViewController.h
+//  MovieMood
+//
+//  Created by Aaron Sky on 2/1/14.
+//  Copyright (c) 2014 Sky Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKYFlipsideViewController : UIViewController
+
+@end
