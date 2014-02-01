f10ade1a6438af6a82bb77772ae2f420f2b9d3a8
Aaron Sky <ans2799@rit.edu>
2014-01-31 22:20:42 -0500

    finished UIKit in the startup, need to test on multiple devices. Also added launch images

--- MovieMood/SKYIntroViewController.h
+++ MovieMood/SKYIntroViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface SKYIntroViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *bucket;
 
 @end
--- MovieMood/SKYIntroViewController.m
+++ MovieMood/SKYIntroViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SKYIntroViewController.h"
+#import <sys/utsname.h>
 
 @interface SKYIntroViewController ()
 
@@ -17,6 +18,7 @@
     UIGravityBehavior* _gravity;
     UICollisionBehavior* _collision;
 }
+@synthesize bucket = _bucket;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,28 +33,44 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    UIView* square = [[UIView alloc] initWithFrame:
-                      CGRectMake(100, 100, 100, 100)];
-    square.backgroundColor = [UIColor grayColor];
-    [self.view addSubview:square];
+    NSMutableArray* smiles = [[NSMutableArray alloc] init];
+    NSLog(@"%@",machineName());
+    int count = ([machineName() isEqualToString:@"iPhone5,1"] || [machineName() isEqualToString:@"iPhone5,2"] || [machineName() isEqualToString:@"x86_64"] ? 50 : 10 );
+    for (int i = 0; i < count; i++) {
+        smiles[i] = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"smile.png"]];
+        [self.view insertSubview:smiles[i] belowSubview:_bucket];
+    }
     
     _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    _gravity = [[UIGravityBehavior alloc] initWithItems:@[square]];
+    _gravity = [[UIGravityBehavior alloc] initWithItems:smiles];
     [_animator addBehavior:_gravity];
     
     _collision = [[UICollisionBehavior alloc]
-                  initWithItems:@[square]];
+                  initWithItems:smiles];
     _collision.translatesReferenceBoundsIntoBoundary = YES;
     [_animator addBehavior:_collision];
     
-    UIDynamicItemBehavior* itemBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[square]];
-    itemBehaviour.elasticity = 0.6;
+    UIDynamicItemBehavior* itemBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:smiles];
+    itemBehaviour.elasticity = 1.0;
     [_animator addBehavior:itemBehaviour];
     
-    UIPushBehavior* pushBehaviour = [[UIPushBehavior alloc] initWithItems:@[square] mode:UIPushBehaviorModeInstantaneous];
-    [pushBehaviour setPushDirection:CGVectorMake(-0.1, 0.1)];
+    UIPushBehavior* pushBehaviour = [[UIPushBehavior alloc] initWithItems:smiles mode:UIPushBehaviorModeInstantaneous];
+    [pushBehaviour setAngle:atan2(-100,0)];
+    [pushBehaviour setMagnitude:10.0];
+    [pushBehaviour setActive:TRUE];
+    [_animator addBehavior:pushBehaviour];
     
     [self performSelector:@selector(goToNextView) withObject:nil afterDelay:3];
+}
+
+NSString*
+machineName()
+{
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    
+    return [NSString stringWithCString:systemInfo.machine
+                              encoding:NSUTF8StringEncoding];
 }
 
 - (void) goToNextView
