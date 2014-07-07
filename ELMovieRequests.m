6ddde01d3cf2f74a3098c91c1a96adbaf341bd5b
Eric <eric.lee.edl@gmail.com>
2014-07-06 21:58:56 -0700

    Prepared request library for Tv shows.

--- ELMovieRequests.m
+++ ELMovieRequests.m
@@ -12,7 +12,7 @@
 
 @implementation ELMovieRequests
 
-+(void) getMoviesWithGenres:(NSArray *)genreList successCallback:(void (^)(id))successCallback failCallBack:(void (^)(NSError *))errorCallback {
++(void) generalMediaRequestWithGenres: (NSArray *)genreList successCallback:(void (^)(id))successCallback failCallBack:(void (^)(NSError *))errorCallback dataStructureCreation: (ELMediaEntity* (^)(id)) creationBlock requestURL: (NSURL *) baseUrl {
     __block int requestsSent = 0;
     __block int requestsRecieved = 0;
     __block BOOL errorCallbackCalled = NO;
@@ -21,9 +21,8 @@
     
     for(NSString *genre in genreList) {
         requestsSent++;
-        NSString *genreURL = [[NSString alloc] initWithFormat:@"%@%@%@", @"https://itunes.apple.com/us/rss/topmovies/limit=100/genre=", genre, @"/json"];
-        NSURL *url = [NSURL URLWithString:genreURL];
-        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+        NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"%@/genre=%@/json", baseUrl, genre]];
+        NSURLRequest *request = [[NSURLRequest alloc] initWithURL: url];
         AFHTTPRequestOperation *requestOperation = [[AFHTTPRequestOperation alloc] initWithRequest: request];
         [requestOperation setResponseSerializer:[AFJSONResponseSerializer serializer]];
         [requestOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
@@ -32,7 +31,7 @@
             id entries = [feed objectForKey:@"entry"];
             NSMutableArray *movies = [[NSMutableArray alloc] init];
             for(id entry in entries) {
-                ELMediaEntity *newMovie = [[ELMovieMediaEntity alloc] initWithEntry:entry];
+                ELMediaEntity *newMovie = creationBlock(entry);
                 [movies addObject:newMovie];
             }
             [data setObject:movies forKey:genre];
@@ -48,6 +47,13 @@
         }];
         [requestOperation start];
     }
+}
+
++(void) getMoviesWithGenres:(NSArray *)genreList successCallback:(void (^)(id))successCallback failCallBack:(void (^)(NSError *))errorCallback {
+    NSURL *url = [NSURL URLWithString: @"https://itunes.apple.com/us/rss/topmovies/limit=100"];
+    [ELMovieRequests generalMediaRequestWithGenres: genreList successCallback: successCallback failCallBack: errorCallback dataStructureCreation:^ELMediaEntity *(id entry) {
+        return [[ELMovieMediaEntity alloc] initWithEntry: entry];
+    } requestURL: url];
 }
 
 +(void) getMovieDetailData:(ELMediaEntity *) movie successCallback:(void (^)(id requestResponse))successCallback failCallBack: (void (^)(NSError * error)) errorCallback {
