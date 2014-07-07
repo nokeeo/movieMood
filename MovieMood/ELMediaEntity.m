3d00afa40b60f54c1958eb77bdef68898e10b5ff
Eric <eric.lee.edl@gmail.com>
2014-07-06 19:26:06 -0700

    Added media entity

---
+++ MovieMood/ELMediaEntity.m
@@ -0,0 +1,24 @@
+//
+//  SKYMovie.m
+//  MovieMood
+//
+//  Created by Eric Lee on 2/13/14.
+//  Copyright (c) 2014 Sky Apps. All rights reserved.
+//
+
+#import "ELMediaEntity.h"
+#import "ELMovieRequests.h"
+
+const NSString *AFFILIATE_KEY = @"11lu3P";
+
+@implementation ELMediaEntity
+
+-(id) initWithEntry:(id)entry {
+    return nil;
+}
+
+-(id) initWithLookupData:(id)entry {
+    return nil;
+}
+
+@end
